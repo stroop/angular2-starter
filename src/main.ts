11da86f636d682dadac13402089edf58142acca2
Jacob Stroop <israel941@gmail.com>
2016-06-17 22:42:31 -0500

    Adds webpack build process

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
+import 'core-js';
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app/app.component';
 
+//noinspection TypeScriptValidateTypes
 bootstrap(AppComponent);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+
+module.exports = {
+
+    entry: './src/main.ts',
+    output: {
+        path: './dist',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders : [
+            {test: /\.ts$/, loader: 'ts'}
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    }
+};
